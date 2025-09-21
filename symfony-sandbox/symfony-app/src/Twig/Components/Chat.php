2c0d97c53b18c36395ca77227fd1107a0f0303f9
muqeetsyed <muqeet@yarikul.in>
2025-09-21 10:08:42 +0530

    Add simple http client openai chat bot

--- symfony-sandbox/symfony-app/src/Twig/Components/Chat.php
+++ symfony-sandbox/symfony-app/src/Twig/Components/Chat.php
@@ -2,21 +2,29 @@
 
 namespace App\Twig\Components;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveArg;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent]
-final class Blog
+final class Chat extends AbstractController
 {
     use DefaultActionTrait;
 
-    public array $messages = ['default message1'];
+    public array $messages = ['Hello from chat bot'];
 
+    public string $chat;
 
     #[LiveAction]
     public function submitMessage(#[LiveArg]string $message): void {
             $this->messages[] = $message;
+    }
+
+
+    #[LiveAction]
+    public function process(): string {
+        return $this->chat;
     }
 }
