044f51111d879726dbdf7d07ec1fa0a1bbd6e5af
muqeetsyed <muqeet@yarikul.in>
2025-10-22 12:13:43 +0530

    Setup turbo, wen encore

--- symfony-sandbox/symfony-app/config/bundles.php
+++ symfony-sandbox/symfony-app/config/bundles.php
@@ -11,4 +11,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
